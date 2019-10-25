41c1b3c3f5b7a494136b9241e740ca8bb0371aff
shuaamunther <shuaamunther123@gmail.com>
2019-10-25 18:47:20 +0300

    navbar

---
+++ src/NavBar.js
@@ -0,0 +1,33 @@
+import React, {Component} from 'react';
+import {
+    StyleSheet, Text, View, TextInput,
+    Button,
+    TouchableHighlight,
+    Image,
+    Alert,
+    KeyboardAvoidingView,
+    ScrollView,
+} from 'react-native';
+
+class LogoTitle extends React.Component {
+    render() {
+        return (
+            <Image source={require('../assets/NavBar.png')}
+                   style={{width: 170, height: 50, marginLeft: 5, marginTop: 7}}/>
+        );
+    }
+}
+
+export default class login extends React.Component {
+    static navigationOptions = {
+        headerTitle: () => <LogoTitle/>,
+    };
+
+    render() {
+        return (
+            <View style={{flex: 2, alignItems: 'center', justifyContent: 'center'}}>
+                <Text>Home Screen</Text>
+            </View>
+        );
+    }
+}
