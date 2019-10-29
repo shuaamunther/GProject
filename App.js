8158c364ad7e18629da983bc82fab04552f8e28e
shuaamunther <shuaamunther123@gmail.com>
2019-10-30 01:14:44 +0200

    edit in app -firebase-

--- App.js
+++ App.js
@@ -37,7 +37,7 @@ export default class App extends React.Component {
     };
 
        if (!firebase.app.length) {
-           firebase.initializeApp({firebaseConfig});
+           firebase.initializeApp(firebaseConfig);
         }
   }
 
