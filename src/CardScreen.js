6792e81051c6f969bc25077cb2ed61a69acb73ab
shuaamunther <shuaamunther123@gmail.com>
2019-11-05 20:27:48 +0200

    Card Screen

--- src/CardScreen.js
+++ src/CardScreen.js
@@ -2,94 +2,8 @@ import React from 'react';
 import { Text, View, StyleSheet ,Image,FlatList} from 'react-native';
 import { Card, Button } from 'react-native-elements';
 
-const DATA = [
-    {
-      id: '1',
-      title: 'Mulukhya',
-      Rate: '5',
-      type :'lunch',
-      img:'https://modo3.com/thumbs/fit630x300/90616/1456142390/%D8%B7%D8%B1%D9%8A%D9%82%D8%A9_%D8%B9%D9%85%D9%84_%D9%85%D9%84%D9%88%D8%AE%D9%8A%D8%A9.jpg'
-    },
-    {
-      id: '2',
-      title: 'Salat',
-      Rate: '4',
-      type:'lunch',
-      img:'https://shamlola.s3.amazonaws.com/Shamlola_Images/5/src/6aad258521393faaba0ce5d7c042c32090880d35.jpg'
-    },
-    {
-      id: '3',
-      title: 'Pizza',
-      Rate: '5',
-      type:'lunch',
-      img:'https://modo3.com/thumbs/fit630x300/51334/1435144381/%D8%B7%D8%B1%D9%8A%D9%82%D8%A9_%D8%B9%D9%85%D9%84_%D8%B9%D8%AC%D9%8A%D9%86%D8%A9_%D8%A7%D9%84%D8%A8%D9%8A%D8%AA%D8%B2%D8%A7_%D8%A7%D9%84%D8%A5%D9%8A%D8%B7%D8%A7%D9%84%D9%8A%D8%A9.jpg'
-    },
-    {
-      id: '4',
-      title: 'Maqluba',
-      Rate: '3',
-      type:'lunch',
-      img:'https://modo3.com/thumbs/fit630x300/197745/1535959216/%D8%B7%D8%B1%D9%8A%D9%82%D8%A9_%D8%B9%D9%85%D9%84_%D9%85%D9%82%D9%84%D9%88%D8%A8%D8%A9_%D8%A7%D9%84%D8%AF%D8%AC%D8%A7%D8%AC.jpg'
-    },
-    
-    {
-      id: '5',
-      title: 'ice cream',
-      Rate: '2',
-      type:'snacks',
-      img:'https://www.hiamag.com/sites/default/files/styles/ph2_960_600/public/recipe/6163561-1815518080.JPG'
-    },
-  
-  ];
-    function Item({ title }) {
-    return (
-      <View style={styles.item}>
-        <Text style={styles.title}>{title}</Text>
-      </View>
-    );
-  }
 class HeaderImageView extends React.Component {
     render() {
-        const DATA = [
-            {
-              id: '1',
-              title: 'Mulukhya',
-              Rate: '5',
-              type :'lunch',
-              img:'https://modo3.com/thumbs/fit630x300/90616/1456142390/%D8%B7%D8%B1%D9%8A%D9%82%D8%A9_%D8%B9%D9%85%D9%84_%D9%85%D9%84%D9%88%D8%AE%D9%8A%D8%A9.jpg'
-              
-            },
-            {
-              id: '2',
-              title: 'Salat',
-              Rate: '4',
-              type:'lunch',
-              img:'https://shamlola.s3.amazonaws.com/Shamlola_Images/5/src/6aad258521393faaba0ce5d7c042c32090880d35.jpg'
-            },
-            {
-              id: '3',
-              title: 'Pizza',
-              Rate: '5',
-              type:'lunch',
-              img:'https://modo3.com/thumbs/fit630x300/51334/1435144381/%D8%B7%D8%B1%D9%8A%D9%82%D8%A9_%D8%B9%D9%85%D9%84_%D8%B9%D8%AC%D9%8A%D9%86%D8%A9_%D8%A7%D9%84%D8%A8%D9%8A%D8%AA%D8%B2%D8%A7_%D8%A7%D9%84%D8%A5%D9%8A%D8%B7%D8%A7%D9%84%D9%8A%D8%A9.jpg'
-            },
-            {
-              id: '4',
-              title: 'Maqluba',
-              Rate: '3',
-              type:'lunch',
-              img:'https://modo3.com/thumbs/fit630x300/197745/1535959216/%D8%B7%D8%B1%D9%8A%D9%82%D8%A9_%D8%B9%D9%85%D9%84_%D9%85%D9%82%D9%84%D9%88%D8%A8%D8%A9_%D8%A7%D9%84%D8%AF%D8%AC%D8%A7%D8%AC.jpg'
-            },
-            
-            {
-              id: '5',
-              title: 'ice cream',
-              Rate: '2',
-              type:'snacks',
-              img:'https://www.hiamag.com/sites/default/files/styles/ph2_960_600/public/recipe/6163561-1815518080.JPG'
-            },
-          
-          ];
         return(
             <View>
             <Image 
@@ -148,8 +62,7 @@ class CardScreen extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
-      marginTop: 100,
-      backgroundColor: '#ecf0f1',
+      marginTop: 8,
       borderRadius:8,
     },
     headerImage:{
