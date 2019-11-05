8b59e4cef279acf3e83ef0ac2a865e8c66dd067f
shuaamunther <shuaamunther123@gmail.com>
2019-11-05 22:49:11 +0200

    get data from firebase

--- src/DataScreen.js
+++ src/DataScreen.js
@@ -7,39 +7,39 @@ import * as firebase from 'firebase';
 const DATA = [
   {
     id: '1',
-    Title: 'Mulukhya',
-    Rate: '5',
-    Type :'lunch',
+    title: 'Mulukhya',
+    rate: '5',
+    type :'lunch',
     img:'https://modo3.com/thumbs/fit630x300/90616/1456142390/%D8%B7%D8%B1%D9%8A%D9%82%D8%A9_%D8%B9%D9%85%D9%84_%D9%85%D9%84%D9%88%D8%AE%D9%8A%D8%A9.jpg'
     
   },
   {
     id: '2',
-    Title: 'Salat',
-    Rate: '4',
-    Type:'lunch',
+    title: 'Salat',
+    rate: '4',
+    type:'lunch',
     img:'https://shamlola.s3.amazonaws.com/Shamlola_Images/5/src/6aad258521393faaba0ce5d7c042c32090880d35.jpg'
   },
   {
     id: '3',
-    Title: 'Pizza',
-    Rate: '5',
+    title: 'Pizza',
+    rate: '5',
     Type:'lunch',
     img:'https://modo3.com/thumbs/fit630x300/51334/1435144381/%D8%B7%D8%B1%D9%8A%D9%82%D8%A9_%D8%B9%D9%85%D9%84_%D8%B9%D8%AC%D9%8A%D9%86%D8%A9_%D8%A7%D9%84%D8%A8%D9%8A%D8%AA%D8%B2%D8%A7_%D8%A7%D9%84%D8%A5%D9%8A%D8%B7%D8%A7%D9%84%D9%8A%D8%A9.jpg'
   },
   {
     id: '4',
-    Title: 'Maqluba',
-    Rate: '3',
+    title: 'Maqluba',
+    rate: '3',
     Type:'lunch',
     img:'https://modo3.com/thumbs/fit630x300/197745/1535959216/%D8%B7%D8%B1%D9%8A%D9%82%D8%A9_%D8%B9%D9%85%D9%84_%D9%85%D9%82%D9%84%D9%88%D8%A8%D8%A9_%D8%A7%D9%84%D8%AF%D8%AC%D8%A7%D8%AC.jpg'
   },
   
   {
     id: '5',
-    Title: 'ice cream',
-    Rate: '2',
-    Type:'snacks',
+    title: 'ice cream',
+    rate: '2',
+    type:'snacks',
     img:'https://www.hiamag.com/sites/default/files/styles/ph2_960_600/public/recipe/6163561-1815518080.JPG'
   },
 
@@ -94,7 +94,7 @@ componentDidMount(){
     <SafeAreaView style={styles.container}>
       <FlatList
             data={DATA}
-            renderItem={({ item }) => <CardScreen cardIteam={item}/>}
+            renderItem={({ item }) => <CardScreen cardItem={item}/>}
             keyExtractor={item => item.id}/>
     </SafeAreaView>
     
